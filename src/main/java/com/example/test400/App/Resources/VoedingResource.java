3aa350e81e3c8e59395c6f62355e12a531bd3835
thoma <thomas-vanderlinden@hotmail.nl>
2022-07-01 01:04:27 +0200

    gerechten gefixt man, blij mee

--- src/main/java/com/example/test400/App/Resources/VoedingResource.java
+++ src/main/java/com/example/test400/App/Resources/VoedingResource.java
@@ -1,11 +1,15 @@
 package com.example.test400.App.Resources;
 
+import com.azure.core.annotation.Get;
 import com.example.test400.App.StartupListener;
 import com.example.test400.Domein.Oefening;
+import com.example.test400.Domein.Recept.Recept;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/voeding")
@@ -17,4 +21,16 @@ public class VoedingResource {
         return StartupListener.recepten;
     }
 
+
+
+    @GET
+    @Path("{naam}")
+    @Produces("application/json")
+    public Response returnRecept(@PathParam("naam") String naam){
+        for(Recept r : StartupListener.recepten){
+            if(r.getNaam().equals(naam)){
+                return Response.ok(r).build();
+            }
+        }return Response.status(404).entity("not found").build();
+    }
 }
--- src/main/webapp/voeding.js
+++ src/main/webapp/voeding.js
@@ -16,29 +16,71 @@ function laatReceptenZien() {
 laatReceptenZien()
 
 
-
 let lijstje = document.getElementById('receptenLijst')
 
-function gerechtInfo(e) {
-    console.log(e)
-    window.location.href="gerechtLatenZien.html";
-}
 
 laatReceptenZien().then(recepten => {
     lijstje.innerHTML = '';
     for (let r of recepten) {
         lijstje.innerHTML += `
 
-
-
-
                               <div class="recepten">
                                    <h2>${r.naam}</h2>
                                    <p>${r.omschrijving}</p>
                                    <img src="${r.plaatje}" height="250px" width="250px">
-                                   <button onclick="gerechtInfo('e')">gerecht bekijken</button>
+                                   <button onclick="vraagGerechtInformatieOp(event, '${r.naam}')">gerecht bekijken</button>
                                   
                               </div>`
     }
 })
+
+//todo: dit nog weghalen hoor:
+//http://jsfiddle.net/minitech/sTLbj/4/
+//https://stackoverflow.com/questions/11128700/create-a-ul-and-fill-it-based-on-a-passed-array
+function makeUL(array) {
+    let list = document.createElement('ul');
+
+    for (let i = 0; i < array.length; i++) {
+        let item = document.createElement('li');
+
+        item.appendChild(document.createTextNode(array[i]));
+
+        list.appendChild(item);
+    }
+    return list;
+}
+
+
+function geefGerechtInformatieWeer(gerecht) {
+    lijstje.innerHTML = '';
+    lijstje.innerHTML += `
+                              <div class="gerecht">
+                                   <h2>${gerecht.naam}</h2>
+                                   <p>${gerecht.omschrijving}</p>
+                                   <img src="${gerecht.plaatje}"  height="250px" width="250px">
+                                   <p>energie: ${gerecht.energie}<br>
+                                    koolhydraten:  ${gerecht.koolhydraten}<br>
+                                    eiwitten: ${gerecht.eiwitten}<br>
+                                    vetten: ${gerecht.vetten}</p>
+                                   <div id="foo"></div>
+                              </div>`
+    document.getElementById('foo').appendChild(makeUL(gerecht));
+
+}
+
+
+function vraagGerechtInformatieOp(event, naam) {
+    event.preventDefault()
+    return fetch('http://localhost:8080/restservices/voeding/' + naam)
+        .then(r => {
+            return r.json()
+        })
+        .then(j => {
+            return j
+        })
+        .then(f => geefGerechtInformatieWeer(f))
+
+
+}
+
 

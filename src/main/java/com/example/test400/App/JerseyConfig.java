3eda647e1acda08f4b51f46ff1842de81596abd2
thoma <thomas-vanderlinden@hotmail.nl>
2022-06-30 11:35:55 +0200

    register RolesAllowed gefixt met toommmm

--- src/main/java/com/example/test400/App/JerseyConfig.java
+++ src/main/java/com/example/test400/App/JerseyConfig.java
@@ -1,10 +1,17 @@
 package com.example.test400.App;
 
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 
 @ApplicationPath("/restservices")
-public class JerseyConfig extends Application {
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig() {
 
+        packages("com.example.test400");
+        register(RolesAllowedDynamicFeature.class);
+    }
 }

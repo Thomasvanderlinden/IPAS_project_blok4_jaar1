a852a9c6b905745dcf19130005a1bbe85b68769d
thoma <thomas-vanderlinden@hotmail.nl>
2022-07-05 19:15:12 +0200

    wel best zo die plaatjes

--- src/main/webapp/blog.js
+++ src/main/webapp/blog.js
@@ -1,28 +1,16 @@
 let lijstje = document.getElementById('mijnLijst')
-let knop = document.getElementById('opsturenBlog')
-let formulier = document.querySelector('#artikelForumuler')
 let heroku = "https://ipasproject.herokuapp.com/"
 let localhost = 'http://localhost:8080/';
 let url = localhost
 
 
-function loginMetFormulier() {
-    if (window.sessionStorage.getItem("myJWT")) {
-        document.forms.artikelForumuler.style = "display:block";
-    } else {
-        document.forms.artikelForumuler.style = "display:none";
-    }
-}
-
-loginMetFormulier()
 function makeUL(array) {
     let list = document.createElement('div');
     for (let i of array) {
         let item = document.createElement('span');
-        item.innerHTML = `<h2>${i.naam}</h2>  <h2>${i.onderwerp}</h2>  <p>${i.tekst}</p> <br>`;
+        item.innerHTML = `<h2>${i.naam}</h2>  <h3>${i.onderwerp}</h3>  <p>${i.tekst}</p> <br><div id="witruimte" style="height: 5px"  ></div><br>`;
         list.appendChild(item)
     }
-
     return list;
 }
 
@@ -46,7 +34,7 @@ function geefinfoWeer() {
 
             lijstje.innerHTML += `<div class="blogArtikelen">
                                    <div id="foo"><br></div>
-</div>`
+                                    </div>`
 
             document.getElementById('foo').appendChild(makeUL(naam));
         }
@@ -57,33 +45,3 @@ function geefinfoWeer() {
 geefinfoWeer()
 
 
-//todo: misschien tips doen voor gebruikers en artikelen voor de admin:
-//todo: knop wit houden als je erop hebt gedrukt
-//todo: hier misschien nog auth invoeren:
-function versturenMeningNaarServer(mening) {
-    if (mening.naam === "" || mening.tekst === "" || mening.onderwerp === "") {
-        return alert("niet alle velden zijn ingevuld!")
-    }
-    return fetch(url + 'restservices/blog', {
-        method: 'POST',
-        body: JSON.stringify(mening),
-        headers: {
-            'Content-Type': 'application/json', "Authorization": "Bearer " + window.sessionStorage.getItem("myJWT")
-        }
-    }).then(geefinfoWeer)
-}
-
-
-knop.addEventListener('click', r => {
-    r.preventDefault()
-
-    let rauweData = new FormData(formulier)
-
-    let mening = {
-        naam: rauweData.get('naam'),
-        onderwerp: rauweData.get('onderwerp'),
-        tekst: rauweData.get('tekst')
-    }
-    versturenMeningNaarServer(mening)
-
-})
